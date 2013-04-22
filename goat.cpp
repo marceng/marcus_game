c55d3b5466b1a48572df59fcc809ee836e6ffde8
Marcus Eng <marcuste@usc.edu>
2013-04-22 15:04:24 -0700

    Completed goat movement.

--- goat.cpp
+++ goat.cpp
@@ -8,27 +8,33 @@ Goat::Goat(QPixmap *i, double x, double y, double lb, double rb, bool direction)
 
 void Goat::move()
 	{
-	if(x == leftBound)
-		{
-		isLeft = false;
-		flipImage();
-		}
-		
-	if(x+width == rightBound)
-		{
-		isLeft = true;
-		flipImage();
-		}
+	handleDirection();
 
 	if(isLeft)
 		{
-		--x;
+		x = x - .5;
 		}
 	
 	else
 		{
-		++x;
+		x = x + .5;
 		}
+	
+	int number = (int) rand() % 4000;
+	if(number < 10)
+		{
+		flipImage();
+		if(isLeft)
+			{
+			isLeft = false;
+			}
 		
+		else
+			{
+			isLeft = true;
+			}
+		}
+	
+	//++y;
 	this->setPos(x, y);
 	}
