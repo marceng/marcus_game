cefc2cd62e7602c4603431dedaa794155c1bdd14
Marcus Eng <marcuseng23@gmail.com>
2013-05-08 18:09:57 -0600

    Update goat.cpp

--- goat.cpp
+++ goat.cpp
@@ -1,3 +1,4 @@
+/*
 Copyright (C) 2013  MARCUS ENG
 
 This program is free software: you can redistribute it and/or modify
@@ -11,6 +12,8 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
 along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 #include "goat.h"
